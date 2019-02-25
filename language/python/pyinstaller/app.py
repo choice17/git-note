9c778761ebba231f72c37f9d453d605442ace9a8
choice17 <choinus4ms@gmail.com>
2019-02-26 00:18:42 +0800

    Revise dynamic import module

--- language/python/pyinstaller/app.py
+++ language/python/pyinstaller/app.py
@@ -1,16 +1,26 @@
 import os
+import imp
 
 FILE = "print_app.py"
 
 def print_main():
 	print("Hello world from print_main!")
 
+
+def importModule(content, filename):
+	"""
+	https://www.oreilly.com/library/view/python-cookbook/0596001673/ch15s03.html
+	"""
+	module = imp.new_module(filename)
+	exec(content, module.__dict__)
+	return module
+
 def main():
 	print_main()
 	if os.path.exists(FILE):
-		import print_app as P
+		fobj = open(FILE,'r').read()
+		P = importModule(fobj, FILE)
 		P.print_app()
-
 
 if __name__ == "__main__":
 	main()
