380412ce64b2603b3118dd3a85736fcbc6054dfe
choice17 <choinus4ms@gmail.com>
2019-02-26 01:35:21 +0800

    Add sample for import and exec from script

--- language/python/pyinstaller/app.py
+++ language/python/pyinstaller/app.py
@@ -20,13 +20,24 @@ def main():
 	if os.path.exists(FILE):
 		fobj = open(FILE,'r').read()
 		P = importModule(fobj, FILE)
-
+		"""
+		import <module> as <P>
+		"""
 		code = """
 a = P.print_app()
 b = P.print_bar()
 print('print a:(%s) and b:(%s) from main' %(a, b))
 """
 		exec(code)
-
+		"""
+		import <module> directly from .py
+		""" 
+		exec(fobj)
+		code = """
+a = print_app()
+b = print_bar()
+print('print a:(%s) and b:(%s) from main' %(a, b))
+"""
+		exec(code)
 if __name__ == "__main__":
 	main()
