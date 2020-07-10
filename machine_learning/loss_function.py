be3913ec37fd44e67e7da0e4c814b051a6a58a47
Choi <tcyu@umich.edu>
2020-07-11 07:13:52 +0800

    Update plot color in loss function

--- machine_learning/loss_function.py
+++ machine_learning/loss_function.py
@@ -23,27 +23,34 @@ def binary_focal_loss_with_logits(gamma=2.0, alpha=0.5):
     return binary_focal_loss
 
 def main():
+    # setup
     R = 1000
+    NS = np.linspace(0,1.0,R)
     a = K.constant(np.vstack([np.ones((1,R)),np.zeros((1,R))]))
-    b=  K.constant(np.vstack([np.expand_dims(np.linspace(0,1.0,R),0),np.expand_dims(np.linspace(0,1.0,R),0)]))
+    b=  K.constant(np.vstack([np.expand_dims(NS,0),np.expand_dims(NS,0)]))
     
+    # create loss
     gammas = [0.5, 1, 2, 3, 4]
     c0_1 = binary_focal_loss_with_logits(0.5, -1)(a, b, 1)
-    c1 = binary_focal_loss_with_logits(1, 0.75)(a, b, 1)
-    c2 = binary_focal_loss_with_logits(2, 0.75)(a, b, 1)
-    c3 = binary_focal_loss_with_logits(3, 0.75)(a, b, 1)
-    c4 = binary_focal_loss_with_logits(4, 0.75)(a, b, 1)
+    c1 = binary_focal_loss_with_logits(1, -1)(a, b, 1)
+    c2 = binary_focal_loss_with_logits(2, -1)(a, b, 1)
+    c3 = binary_focal_loss_with_logits(3, -1)(a, b, 1)
+    c4 = binary_focal_loss_with_logits(4, -1)(a, b, 1)
     ce = keras.backend.binary_crossentropy(a, b)
     
-    d = [np.clip(K.eval(c),0,5) for c in [c0_1, c1, c2, c3, c4]]
-    dce = np.clip(K.eval(ce),0,5)
+    d = [K.eval(c) for c in [c0_1, c1, c2, c3, c4]]
+    dce = K.eval(ce)
     
+    # plot
     import matplotlib.pyplot as plt
     for i in range(5):
-        plt.plot(d[i][0], label='focal_loss[%.1f,noweight]' % gammas[i])
-    plt.plot(dce[0],color='orange',label='cross_entropy')
+        plt.plot(NS, d[i][0], label='focal_loss[%.1f,noweight]' % gammas[i])
+    plt.plot(NS, dce[0],color='black',label='cross_entropy')
+    plt.ylim([0,5])
     plt.legend()
+    plt.title("binary loss on ground truth is 1")
     plt.show()
+    # we can observe the curve is similar to the one in the paper
 
 if __name__ == '__main__':
     main()
