1ef5fa99bbe9c2d66c7e989b66712ba03f8d064b
Choi <tcyu@umich.edu>
2020-11-13 16:34:20 +0800

    Fix cpp on c

--- language/c/cpp_on_c/opencv/src/opencv.cc
+++ language/c/cpp_on_c/opencv/src/opencv.cc
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-CASCADE::add(int w, int h)
+int CASCADE::add(int w, int h)
 {
     cout << __func__ << " " << __LINE__ << \
     "return: " << w + h << endl;
     return w + h; 
 }
 
-MAT::sum(int w, int h)
+int MAT::sum(int w, int h)
 {
     cout << __func__ << " " << __LINE__ << \
     "return: " << w + h << endl;
